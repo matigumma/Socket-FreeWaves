b8882a6698feac6832dd5e0385d23206fbe970c3
Ramiro Gumma <ramirogumma@hotmail.com>
2022-12-08 12:04:42 -0300

    for production

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/de
 var _express = _interopRequireDefault(require("express"));
 var _socket = require("socket.io");
 var _http = _interopRequireDefault(require("http"));
+var _config = require("./config.js");
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { (0, _defineProperty2["default"])(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
 var app = (0, _express["default"])();
@@ -30,6 +31,6 @@ io.on("connection", function (socket) {
   //   });
 });
 
-httpServer.listen(3000, function () {
-  console.log("server en el puerto", 3000);
+httpServer.listen(_config.PORT, function () {
+  console.log("server en el puerto", _config.PORT);
 });
